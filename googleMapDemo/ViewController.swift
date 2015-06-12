5a9501ca7ab54e57de6e5a4ec6d7567bb8263f3b
Chieh-Chun Li <whplue07@Chieh-ChundeMacBook-Pro.local>
2015-06-13 00:58:15 +0800

    Initial Commit

---
+++ googleMapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  googleMapDemo
+//
+//  Created by Chieh-Chun Li on 2015/6/13.
+//  Copyright (c) 2015年 Dan.Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
